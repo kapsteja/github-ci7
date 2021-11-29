1d8f813fbed1a959d4f808bb82f644e061c8c141
kapsteja <teja@kapstonellc.com>
2021-11-29 19:13:49 +0530

    teja commited

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Teja tutorial
         </a>
       </header>
     </div>
