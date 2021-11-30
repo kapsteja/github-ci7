4de09377988f3bd0587b855929720fad115887ae
kapsteja <teja@kapstonellc.com>
2021-11-30 18:35:01 +0530

    teja commited

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://www.w3schools.com"
+          href="https://www.wikipedia.org"
           target="_blank"
           rel="noopener noreferrer"
         >
